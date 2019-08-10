3e938aa54ff36fc4c39d21a1ca98e06e61dcead5
choudharyamit26 <ravichoudhary766@gmail.com>
2019-08-10 21:03:45 +0530

    First commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\choudhary's\desktop\python-practice\recipe\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
